82d196c3137ac0aa89cbda8df94a318fc2295480
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2022-12-26 10:11:09 -0500

    add basic data /details page / base components

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import CoachDetails from './pages/coaches/CoachDetails.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/CoachDetails.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 const router = createRouter({
@@ -13,6 +13,7 @@ const router = createRouter({
     { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
+      props: true,
       component: CoachDetails,
       children: [{ path: 'contact', component: ContactCoach }], // /coaches/c1/contact
     },
